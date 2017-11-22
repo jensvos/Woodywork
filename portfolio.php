51a5c89a867268048e8fbfd934752105468d43eb
ridewo <33694236+ridewo@users.noreply.github.com>
2017-11-22 09:33:58 +0100

    Add files via upload

--- portfolio.php
+++ portfolio.php
@@ -19,13 +19,48 @@
             </ul>
         </nav>
         <div class="container">
-            <div class="grid_portfolio">
-                <div class="p1"><h2>Titel</h2><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p></div>
-                <div class="p2"><h2>Titel</h2><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p></div>
-                <div class="p3"></div>
-                <div class="p4"><h2>Titel</h2><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p></div>
-                <div class="p5"><h2>Titel</h2><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p></div>
-            </div>
+            <?php
+            //maak een variable voor het aantal producten
+            //loop door de producten en schrijf voor elk product div + class h2 en p
+            //voeg voor elke vier producten een extra div toe voor de sier
+            //voeg voor elke vier producten+extra div een container div eromheen toe
+            
+            
+            
+            
+            //aantal producten uit de database:
+            $aantal =23;
+            
+            //bereken aantal sier divjes dat moet worden gemaakt
+            $aantal+=floor(($aantal/4)); //voor elke vier divjes een extra
+            if($aantal%5>1){
+                $aantal++;
+            }
+            
+            for($i = 0; $i<$aantal;$i++){
+                
+                if($i%5==0){
+                    print('<div class="grid_portfolio">'); //container div voor elke vijf divjes
+                }
+                
+                print('<div class="p'.($i%5+1).'">'); 
+                if($i%5!=2){//sla elke derde over die heeft geen inhoud
+
+                    //haal hier spul uit de database
+                    //print de inhoud van het divje
+                    print('<h2>Titel</h2><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>');
+                }
+                print('</div>');//sluit div af
+                
+                if($i%5==4||$i==$aantal-1){ // sluit container div af na vijf divjes of na de laatste div
+                    print('</div>');
+                }
+                
+            }
+        
+           
+            ?>
+            
         </div>
         <footer>
             <div class="grid_footer">
